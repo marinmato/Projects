4a8555b1bf01512f3851e84eb36138361017e32a
marinmato <65510557+marinmato@users.noreply.github.com>
2025-02-04 12:39:22 -0500

    Contains the src portion of the app
    I am still in the process of developing the app. I have uploaded the src portion of the app, which contains all the pages and the design. I am utilizing the Ionic and Angular framework, which will make the app available on web, Android and iOS.

---
+++ src/app/pages/reset-password/reset-password.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reset-password',
+  templateUrl: './reset-password.page.html',
+  styleUrls: ['./reset-password.page.scss'],
+  standalone: false,
+})
+export class ResetPasswordPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  firebaseConfig : {
+    apiKey: "AIzaSyBCd-NOM8qQFemeW8gODYcUhGFho77MpmI",
+    authDomain: "heelridev1.firebaseapp.com",
+    projectId: "heelridev1",
+    storageBucket: "heelridev1.firebasestorage.app",
+    messagingSenderId: "901643731425",
+    appId: "1:901643731425:web:a75bbcd116e304e863d2a0",
+    measurementId: "G-J3GCKC76HH"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
